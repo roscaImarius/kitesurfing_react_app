a9b48fd6b4c5c382d42221445635fb2906919a19
marius <roscaimarius@gmail.com>
2021-03-20 20:36:43 +0200

    deleted history

--- .history/src/components/TableHeader_20210320132542.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const TableHeader = () => {
-  return <div>table header</div>;
-};
-
-export default TableHeader;
--- .history/src/components/TableHeaders_20210320133419.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const TableHeaders = () => {
-  return <div></div>;
-};
-
-export default TableHeaders;
