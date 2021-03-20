6230bbd3fd112b8e8a484f5f6cbe9d8f5f90999f
marius <roscaimarius@gmail.com>
2021-03-20 21:21:14 +0200

    working on sorting

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div>Kite</div>
+      <button>ADD SPOT</button>
+    </div>
+  );
+};
+
+export default Header;
